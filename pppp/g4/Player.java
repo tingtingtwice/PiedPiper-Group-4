c8b44afbaa23f094002834bcff8f2335442af4a1
Rhea Goel <rg2936@columbia.edu>
2015-09-20 20:16:28 -0400

    Error free code

--- pppp/g4/Player.java
+++ pppp/g4/Player.java
@@ -17,6 +17,7 @@ public class Player implements pppp.sim.Player {
 	private int dst_no = 0;
 	private int total_regions = 0;
 	Boolean[] completed_sweep = null;
+    private Cell[] grid = null;
 
 	// create move towards specified destination
 	private static Move move(Point src, Point dst, boolean play) {
@@ -51,7 +52,7 @@ public class Player implements pppp.sim.Player {
 		completed_sweep = new Boolean[n_pipers];
 		Arrays.fill(completed_sweep, Boolean.FALSE);
 
-
+        this.grid = create_grid(this.side);
 
 		for (int p = 0; p != n_pipers; ++p) {
 			// spread out at the door level
@@ -62,7 +63,7 @@ public class Player implements pppp.sim.Player {
 			boolean swap = id == 1 || id == 3;
 			Point before_gate = point(door, side * 0.5 * .85, neg_y, swap);
 			Point inside_gate = point(door, side * 0.5 * 1.2, neg_y, swap);// first and third position is at the door
-			double distance = side / 2;
+			double distance = side / 3;
 			double theta = Math.toRadians(p * 90.0 / (n_pipers - 1) + 45);
 
 			pos[p][0] = point(door, side * 0.5, neg_y, swap);
@@ -85,42 +86,270 @@ public class Player implements pppp.sim.Player {
 		}
 	}
 
+    public Cell[] create_grid(int side) {
+        /*
+         Returns a Cell[] array of length = number of cells = side/20 * side/20
+         */
+        int cell_side = 0;
+        if (side % 20 == 0)
+            cell_side = side/20;
+        float half = side/2;
+        Cell[] grid = new Cell[cell_side*cell_side];
+        
+        for(int i=0; i < cell_side; i++) {
+            for(int j=0; j < cell_side; j++) {
+                Cell cell = new Cell(
+                                     new Point(  // X, Y - center
+                                               (i + 0.5) * 20 - half,
+                                               (j + 0.5) * 20 - half
+                                               ));
+                grid[(i * cell_side) + j] = cell;
+            }
+        }
+        
+        Cell.counter = 0;
+        return grid;
+    }
+    
+    public void display_grid() {
+        for (int i=0; i < this.grid.length; i++) {
+            this.grid[i].display_cell();
+            System.out.println();
+        }
+    }
+    
+    public Cell find_cell(Point rat) {
+        for (int i=0; i<this.grid.length; i++) {
+            Cell cell = this.grid[i];
+            double x1 = cell.center.x - cell.side/2;
+            double x2 = cell.center.x + cell.side/2;
+            double y1 = cell.center.y + cell.side/2;
+            double y2 = cell.center.y - cell.side/2;
+            
+            if (rat.x >= x1 && rat.x <= x2 && rat.y >= y2 && rat.y <= y1) {
+                return cell;
+            }
+        }
+        return null;
+    }
+    
+    public void update_grid_weights(Point[] rats) {
+        for (int i=0; i < this.grid.length; i++) {
+            this.grid[i].weight = 0;
+        }
+        
+        for (Point rat: rats) {
+            System.out.println("Rat is at: " + rat.x + ", " + rat.y);
+            Cell cell = find_cell(rat);
+            System.out.println("Rat found at: " + cell.center.x + ", " + cell.center.y);
+
+            if (cell != null)
+                cell.weight++;
+        }
+    }
+    
+    public Map<Integer, Point> get_piper_to_cell(int remaining_pipers) {
+        Cell[] grid_copy = Arrays.copyOf(grid, grid.length);
+        for (int j=0; j < grid.length; j++) {
+            // System.out.println(grid[j].weight + " " + grid_copy[j].weight);
+        }
+        // new Cell[grid.length];
+        // for (int j=0; j<grid.length; j++) {
+        // 	grid_copy[j] = grid[j];
+        // }
+        
+        Map<Integer, Point> piper_to_cell = new HashMap<Integer, Point>();
+        List<Integer> all_pipers = new ArrayList<Integer>();
+        
+        for (int i=0; i<remaining_pipers; i++) {
+            all_pipers.add(i);
+        }
+        
+        int i;
+        int cells_to_consider;
+        int sum;
+        int avg;
+        int n_p_to_i;
+        int piper;
+        List<Cell> non_zero_cells = new ArrayList<Cell>();
+        Iterator<Cell> iter_list;
+
+        for (int k=0; k < grid_copy.length; k++) {
+            non_zero_cells.add(grid_copy[k]);
+        }
+        
+        cells_to_consider = remaining_pipers;
+        while (remaining_pipers > 0) {
+
+            // cells_to_consider = remaining_pipers;
+
+            int prev_length = non_zero_cells.size();
+            if (prev_length > cells_to_consider) {
+                // System.out.println("$$$$$$$$$$$$$$$$$$$$$$$"+prev_length);
+                // for (int k = cells_to_consider; k < prev_length; k++) {
+                //     System.out.println("k: "+ k);
+                //     non_zero_cells.remove(k);
+                //     non_zero_cells.trimToSize();
+                // }
+                non_zero_cells = non_zero_cells.subList(0, cells_to_consider);
+            }
+            
+            sum = 0;
+
+
+            System.out.println("1 Remaining: " + remaining_pipers);
+            System.out.println("1 Cells to consider: " + cells_to_consider);
+            System.out.println("1 Non zero cells: " + non_zero_cells.size());
+            for (int k=0; k<non_zero_cells.size(); k++) {
+                System.out.println(non_zero_cells.get(k).weight);
+            }
+            
+            iter_list = non_zero_cells.iterator();
+            for (i=0; i<cells_to_consider; i++) {
+                // System.out.println("Computing avg...");
+                // if (grid_copy[i].weight != 0) {
+                //     non_zero_cells.append(grid_copy[i])
+                //     sum += grid_copy[i].weight;
+
+                // }
+                Cell next_item = iter_list.next();
+                 if (next_item.weight != 0) {
+                    sum += next_item.weight;
+                }
+                else
+                {
+                    System.out.println("Before: " + non_zero_cells.size());
+                    iter_list.remove();
+                    System.out.println("After: " + non_zero_cells.size());
+                }
+            }
+            cells_to_consider = non_zero_cells.size();
+            if (cells_to_consider == 0)
+                break;
+            System.out.println("2 Remaining: " + remaining_pipers);
+            System.out.println("2 Cells to consider: " + cells_to_consider);
+
+            avg = sum/cells_to_consider;
+            // if (avg < 1) avg = 1;
+            System.out.println("Avg: " + avg);
+            
+            i = 0;
+            iter_list = non_zero_cells.iterator();
+            Cell this_cell;
+            while(i < cells_to_consider) {
+                // System.out.println("i: " + i);
+                if (iter_list.hasNext())
+                    this_cell = iter_list.next();
+                else 
+                    break;
+                
+                // n_p_to_i = grid_copy[i].weight/avg;
+                n_p_to_i = this_cell.weight/avg;
+                System.out.println("n p i: " + n_p_to_i);
+                
+                Iterator<Integer> iter = all_pipers.iterator();
+                for (int j=0; j<n_p_to_i; j++) {
+                    if (iter.hasNext())
+                    {
+                        piper = iter.next();
+                        piper_to_cell.put(piper, this_cell.center);
+                        iter.remove();
+                        remaining_pipers -= 1;
+                    }
+                }
+                this_cell.weight = this_cell.weight % avg;
+                if (this_cell.weight == 0){
+                    System.out.println("Before: " + non_zero_cells.size());
+                    iter_list.remove();
+                    System.out.println("After: " + non_zero_cells.size());
+                }
+                cells_to_consider = non_zero_cells.size();
+                if (cells_to_consider == 0)
+                    break;
+                i++;
+                
+            }
+            
+        }
+
+        while (remaining_pipers > 0) {
+            Iterator<Integer> iter = all_pipers.iterator();
+            if (iter.hasNext()) {
+                    piper = iter.next();
+                    piper_to_cell.put(piper, grid_copy[0].center);
+                    iter.remove();
+                    remaining_pipers -= 1;
+            }
+        }
+                
+        // for (Map.Entry<Integer, Point> entry : piper_to_cell.entrySet()) {
+        //     int key = entry.getKey();
+        //     Point value = entry.getValue();
+        //     System.out.println(key + " " + value.x + " " + value.y);
+        // }
+        
+        return piper_to_cell;
+        
+    }
+
 	// return next locations on last argument
 	public void play(Point[][] pipers, boolean[][] pipers_played,
 					 Point[] rats, Move[] moves) {
-		//p : is the index of piper for current player
-		for (int p = 0; p != pipers[id].length; ++p) {
-			Point src = pipers[id][p];
-			Point dst = pos[p][pos_index[p]];
+        
+        try {
+            Map<Integer, Point> piper_to_cell = null;
+            update_grid_weights(rats);
+            
+            // sort the cells in the Cell[] grid in descending order of weight/number_of_rats
+            Arrays.sort(this.grid, Collections.reverseOrder());
+            piper_to_cell = get_piper_to_cell(pipers[id].length);
+            for (Map.Entry<Integer, Point> entry : piper_to_cell.entrySet()) {
+                int key = entry.getKey();
+                Point value = entry.getValue();
+                System.out.println(key + " " + value.x + " " + value.y);
+            }
 
-			if (completed_sweep[p] && pos_index[p] == 1)
-			{
-				pos_index[p] = 4;
-//				dst = null; // call new destination function here
-//				System.out.println("Play loop 2"+completed_sweep+" | is it null : "+dst+" pos_index "+pos_index[p]);
-			}
-			// if null then get random position
-			if (dst == null) dst = random_pos[p];
-			// if position is reached
-			if (Math.abs(src.x - dst.x) < 0.000001 &&
-					Math.abs(src.y - dst.y) < 0.000001) {
-				// discard random position
-				if (dst == random_pos[p]) random_pos[p] = null;
-				// get next position
-				if (++pos_index[p] == pos[p].length){
-					pos_index[p] = 0;
-					completed_sweep[p] = true;
-				}
-				dst = pos[p][pos_index[p]];
-				// generate a new position if random
-				if (dst == null) {
-					double x = (gen.nextDouble() - 0.5) * side * 0.9;
-					double y = (gen.nextDouble() - 0.5) * side * 0.9;
-					random_pos[p] = dst = new Point(x, y);
-				}
-			}
-			// get move towards position
-			moves[p] = move(src, dst, pos_index[p] > 1);
-		}
+            //p : is the index of piper for current player
+            for (int p = 0; p != pipers[id].length; ++p) {
+                Point src = pipers[id][p];
+                Point dst = pos[p][pos_index[p]];
+                
+                if (completed_sweep[p] && pos_index[p] == 1)
+                {
+                    pos_index[p] = 4;
+                    //				dst = null; // call new destination function here
+                }
+                // if null then get random position
+                if (dst == null) dst = random_pos[p];
+                // if position is reached
+                if (Math.abs(src.x - dst.x) < 0.000001 &&
+                    Math.abs(src.y - dst.y) < 0.000001) {
+                    // discard random position
+                    if (dst == random_pos[p]) random_pos[p] = null;
+                    // get next position
+                    if (++pos_index[p] == pos[p].length){
+                        pos_index[p] = 0;
+                        completed_sweep[p] = true;
+                    }
+                    dst = pos[p][pos_index[p]];
+                    // generate a new position if random
+                    if (dst == null || pos_index[p] == 5) {
+                        //					double x = (gen.nextDouble() - 0.5) * side * 0.9;
+                        //					double y = (gen.nextDouble() - 0.5) * side * 0.9;
+                        random_pos[p] = dst = piper_to_cell.get(id);
+                    }
+                }
+
+                // if (pos_index[p] == 5) {
+                //     dst = piper_to_cell.get(id);
+                // }
+                // get move towards position
+                moves[p] = move(src, dst, (pos_index[p] > 1 && pos_index[p] < 4) || (pos_index[p] > 5));
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        
 	}
 }
